5731169d47a3e5444ebb429577a2b3c900615842
Emilia <correoalcantaraemilia@gmail.com>
2023-07-13 17:52:55 -0400

    update diseño primer fragmento y agregué un par más de imagenes

--- app/src/main/java/cl/individual/recyclerphotos/GalleryFragment.java
+++ app/src/main/java/cl/individual/recyclerphotos/GalleryFragment.java
@@ -80,15 +80,29 @@ public class GalleryFragment extends Fragment{
 
     private List<PhotoModel> getData() {
         ArrayList<PhotoModel> photoGallery = new ArrayList<>();
-        photoGallery.add(new PhotoModel("https://images.unsplash.com/photo-1689072503598-638956beee7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=660&q=80", "Una inglesia en el medio de un campo bajo el cielo nocturno estrellado"));
+        photoGallery.add(new PhotoModel("https://images.unsplash.com/photo-1689072503598-638956beee7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=660&q=80",
+                "Una iglesia en el medio de un campo bajo el cielo nocturno estrellado"));
 
-        photoGallery.add(new PhotoModel("https://images.unsplash.com/photo-1682687220742-aba13b6e50ba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxMXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60", "Foto de una persona parada en la cima de una colina rocosa"));
+        photoGallery.add(new PhotoModel("https://images.unsplash.com/photo-1682687220742-aba13b6e50ba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxMXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+                "Foto de una persona parada en la cima de una colina rocosa"));
 
-        photoGallery.add(new PhotoModel("https://images.unsplash.com/photo-1688725755373-10d1bb23b55d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80", "Una persona caminando por una calle sosteniendo un paraguas"));
+        photoGallery.add(new PhotoModel("https://images.unsplash.com/photo-1688725755373-10d1bb23b55d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+                "Una persona caminando por una calle sosteniendo un paraguas"));
 
-        photoGallery.add(new PhotoModel("https://images.unsplash.com/photo-1689089764982-3c081cc0089a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0NXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60", "Una mujer parada frente a una cascada"));
+        photoGallery.add(new PhotoModel("https://images.unsplash.com/photo-1689089764982-3c081cc0089a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0NXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+                "Una mujer parada frente a una cascada"));
 
-        photoGallery.add(new PhotoModel("https://images.unsplash.com/photo-1688890239467-c43da335fe7e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1MHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60", "Un pequeño animal parado en un campo de flores blancas"));
+        photoGallery.add(new PhotoModel("https://images.unsplash.com/photo-1688890239467-c43da335fe7e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1MHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+                "Un pequeño animal parado en un campo de flores blancas"));
+
+        photoGallery.add(new PhotoModel("https://images.unsplash.com/photo-1500862321-177c80d8ca32?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGJlZXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+                "Fotografía macro de una abeja"));
+
+        photoGallery.add(new PhotoModel("https://images.unsplash.com/photo-1688999558024-0bbeac1c0102?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTEzfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60",
+                "El primer plano de la cara de una leona"));
+
+        photoGallery.add(new PhotoModel("https://images.unsplash.com/photo-1688732252178-d73dddea8464?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MzE3fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60",
+                "Un pequeño pajaro colorido sentado en una rama en la oscuridad"));
 
         return photoGallery;
     }
